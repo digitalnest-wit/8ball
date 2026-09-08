d4cb999f6f8d810b785c627088dfe971ccc305fc
Leonardo Villalobos <villaleobos@gmail.com>
2026-09-08 10:45:47 -0700

    move files out of assets and fix css positioning

--- script.js
+++ script.js
@@ -4,19 +4,25 @@ const submitBtn = document.getElementById("submit-btn");
 const ball = document.getElementById("ball");
 const ballVoid = document.getElementById("ball-void");
 
-const responsePool = [
+const responses = [
   "Don't count on it.",
   "Concentrate and ask again.",
   "Yes.",
   "No.",
   "Maybe.",
-  "I think you're cooked."
+  "You're cooked."
 ];
 
 result.classList.add("hidden");
 ballVoid.classList.add("hidden");
 
-submitBtn.addEventListener('click', function () {
+submitBtn.addEventListener('click', function (event) {
+  // prevent the page from being redirected when the 
+  // button is clicked.
+  event.preventDefault();
+
+  // hide the "ball void" image and show the "ball" when
+  // there is no question being asked.
   if (question.value.length == 0) {
     ballVoid.classList.add("hidden");
     ball.classList.remove("hidden");
@@ -24,11 +30,13 @@ submitBtn.addEventListener('click', function () {
     return;
   }
 
+  // show the "ball void" image and result element.
   ballVoid.classList.remove("hidden");
   ball.classList.add("hidden");
   result.classList.remove("hidden");
 
-  let index = Math.floor(Math.random() * responsePool.length);
-  let response = responsePool[index];
+  // pick a random response.
+  let index = Math.floor(Math.random() * responses.length);
+  let response = responses[index];
   result.innerHTML = response;
 });
